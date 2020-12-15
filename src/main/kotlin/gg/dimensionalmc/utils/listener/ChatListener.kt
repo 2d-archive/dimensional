991c7f431cebb57dd684fa7fda92cf3c4eb80cd4
LolWastedJS <ml2dofficial@gmail.com>
2020-12-14 19:39:33 -0800

    make the chat webhook look a little nicer

--- src/main/kotlin/gg/dimensionalmc/utils/listener/ChatListener.kt
+++ src/main/kotlin/gg/dimensionalmc/utils/listener/ChatListener.kt
@@ -1,9 +1,13 @@
 package gg.dimensionalmc.utils.listener
 
+import club.minnced.discord.webhook.send.AllowedMentions
+import club.minnced.discord.webhook.send.WebhookMessageBuilder
 import gg.dimensionalmc.utils.Dimensional
+import gg.dimensionalmc.utils.Dimensional.Companion.escape
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
+import java.util.*
 
 class ChatListener(private val plugin: Dimensional) : Listener {
   @EventHandler
@@ -12,6 +16,20 @@ class ChatListener(private val plugin: Dimensional) : Listener {
       return
     }
 
-    plugin.webhookClient!!.send("**${Dimensional.escape(evt.player.name)}**: ${Dimensional.escape(evt.message)}")
+    val message = WebhookMessageBuilder()
+      .setUsername(evt.player.name)
+      .setAvatarUrl(getPlayerAvatar(evt.player.uniqueId))
+      .setContent(escape(evt.message))
+      .setAllowedMentions(AllowedMentions.none())
+
+    plugin.webhookClient!!.send(message.build())
+  }
+
+  companion object {
+    /**
+     * Returns the URL of a player's avatar.
+     * @param uuid The player's ID.
+     */
+    fun getPlayerAvatar(uuid: UUID) = "https://minotar.net/avatar/$uuid"
   }
 }
