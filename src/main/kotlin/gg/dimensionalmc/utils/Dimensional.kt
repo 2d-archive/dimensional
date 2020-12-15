5ca8fef0eb7bd970d7783e43d4fb1fbc9f71bb83
LolWastedJS <ml2dofficial@gmail.com>
2020-12-14 19:38:08 -0800

    remove events from the main file

--- src/main/kotlin/gg/dimensionalmc/utils/Dimensional.kt
+++ src/main/kotlin/gg/dimensionalmc/utils/Dimensional.kt
@@ -4,20 +4,12 @@ import club.minnced.discord.webhook.WebhookClient
 import gg.dimensionalmc.utils.commands.DimensionalCommand
 import gg.dimensionalmc.utils.commands.DiscordCommand
 import gg.dimensionalmc.utils.listener.ChatListener
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.TextComponent
-import net.kyori.adventure.text.format.TextColor
-import net.kyori.adventure.text.format.TextDecoration
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
-import org.bukkit.Bukkit
+import gg.dimensionalmc.utils.listener.ServerListener
 import org.bukkit.ChatColor
-import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.server.ServerListPingEvent
 import org.bukkit.plugin.java.JavaPlugin
-import java.io.File
 
-class Dimensional : JavaPlugin(), Listener {
+class Dimensional : JavaPlugin() {
   var webhookClient: WebhookClient? = null
     private set
 
@@ -26,9 +18,6 @@ class Dimensional : JavaPlugin(), Listener {
   private val isChatLoggingEnabled: Boolean
     get() = !config.getString("chat-webhook.token")
       .isNullOrEmpty()
-
-  private val serializer: LegacyComponentSerializer =
-    LegacyComponentSerializer.builder().hexColors().useUnusualXRepeatedCharacterHexFormat().build()
 
   override fun onEnable() {
     if (isChatLoggingEnabled) {
@@ -41,7 +30,7 @@ class Dimensional : JavaPlugin(), Listener {
       logger.info("No webhook token specified.")
     }
 
-    listOf(this, ChatListener(this))
+    listOf(ServerListener(this), ChatListener(this))
       .forEach { this.server.pluginManager.registerEvents(it, this) }
 
     listOf(DiscordCommand(this), DimensionalCommand(this))
@@ -53,34 +42,6 @@ class Dimensional : JavaPlugin(), Listener {
       webhookClient?.close()
       logger.info("Closed Webhook Client.")
     }
-  }
-
-  @EventHandler
-  fun onServerListPing(evt: ServerListPingEvent) {
-    val mainColor = this.config.getString("main-color")!!
-
-    val motd: TextComponent.Builder = Component.text()
-      .append(Component.text("Welcome to "))
-      .append(
-        Component.text("Dimensional")
-          .color(TextColor.fromHexString(mainColor))
-          .decoration(TextDecoration.BOLD, true)
-      )
-      .append(Component.newline())
-      .append(Component.text("Have fun! "))
-
-    val discord = config.getString("links.discord")
-    if (discord != null) {
-      motd.append(
-        Component.text(discord)
-          .color(TextColor.fromHexString(mainColor))
-          .decorate(TextDecoration.ITALIC)
-          .decorate(TextDecoration.UNDERLINED)
-      )
-    }
-
-    evt.motd = serializer.serialize(motd.build())
-    evt.setServerIcon(Bukkit.loadServerIcon(File("server-icon.png")))
   }
 
   companion object {
